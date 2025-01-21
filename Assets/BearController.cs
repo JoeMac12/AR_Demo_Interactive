a694166e7f731e010d24b6e57f05b844cf23886f
Arman <144450534+JoeMac12@users.noreply.github.com>
2025-01-21 01:11:44 -0400

    Bear Animation When moving

--- Assets/BearController.cs
+++ Assets/BearController.cs
@@ -8,16 +8,18 @@ public class BearController : MonoBehaviour
 
 	private FixedJoystick fixedJoystick;
 	private Rigidbody rigidBody;
+	private Animator animator;
 
 	private void OnEnable()
 	{
 		fixedJoystick = FindObjectOfType<FixedJoystick>();
 		rigidBody = gameObject.GetComponent<Rigidbody>();
+		animator = gameObject.GetComponent<Animator>();
 	}
 
 	private void FixedUpdate()
 	{
-		if (fixedJoystick == null || rigidBody == null)
+		if (fixedJoystick == null || rigidBody == null || animator == null)
 		{
 			Debug.LogError("Things are broken");
 			return;
@@ -29,7 +31,12 @@ public class BearController : MonoBehaviour
 		Vector3 movement = new Vector3(xVal, 0, yVal);
 		rigidBody.velocity = movement * speed;
 
+		float currentSpeed = rigidBody.velocity.magnitude;
+		animator.SetFloat("Speed", currentSpeed);
+
 		if (xVal != 0 || yVal != 0)
+		{
 			transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(xVal, yVal) * Mathf.Rad2Deg, transform.eulerAngles.z);
+		}
 	}
 }
